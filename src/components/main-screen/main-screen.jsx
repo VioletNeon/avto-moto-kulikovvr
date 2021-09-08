3cdd85869cb619c4f9f742062492f6c463c5348e
Valeriy <KulikovVR@gmail.com>
2021-09-08 23:09:44 +0400

    Подготовка проекта: нарезка графики, шрифты, компоненты

---
+++ src/components/main-screen/main-screen.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function MainScreen() {
+
+    return (
+      <>
+        <main>
+          <h1>AVTO-MOTO</h1>
+        </main>
+      </>
+    );
+}
+
+export default MainScreen;
